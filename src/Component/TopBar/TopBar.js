772f1a19227df0ca25b3bb2e30e600ce99e58163
arienwen <wenbay66@yahoo.com.tw>
2021-05-21 10:46:20 +0800

    05/21 add TopBar End

--- src/Component/TopBar/TopBar.js
+++ src/Component/TopBar/TopBar.js
@@ -5,14 +5,18 @@ import Btn from "../Btn/Btn"
 import FormatListBulletedRoundedIcon from '@material-ui/icons/FormatListBulletedRounded';
 import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
+import LockIcon from '@material-ui/icons/Lock';
+import RoomServiceIcon from '@material-ui/icons/RoomService';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 const Container = styled.div`
     max-height: 34px;
     min-height: 34px;
     background: rgb(1,121,191);
-    padding: 4px;
+    padding: 4px 8px 4px 8px;
     display: flex;
     align-items: center;
+    justify-content: space-between;
 `
 const TitleBtn = styled.span`
     font-weight: 700;
@@ -26,35 +30,67 @@ const Border = styled.span`
     border-left-width: 1px;
     height: 16px;
     margin-left: 8px;
-    margin-right: 8px
+    margin-right: 8px;
+`
+const ContainerDIV = styled.div`
+  display: flex;
+  align-items: center;
 `
 
 export default function TopBar(){
-    const ccc = ()=>{
-        alert('123')
-    }
-    return (
-        <>
-            <Container>
-                <span onClick={ccc}>
-                    <Btn>
-                        <FormatListBulletedRoundedIcon style={{fontSize:14}}/>
-                        <span>看板</span>
-                        <KeyboardArrowDownRoundedIcon/>
-                    </Btn>
-                </span>
-                <span onClick={ccc} style={{marginLeft:'4px'}}>
-                    <Btn>
-                        <TitleBtn>程式roadMap</TitleBtn>
-                    </Btn>
-                </span>
-                <span style={{marginLeft:'4px'}}>
-                    <Btn>
-                        <StarBorderOutlinedIcon />
-                    </Btn>
-                </span>
-                <Border />
-            </Container>
-        </>
-    )
+  const ccc = ()=>{
+      alert('123')
+  }
+  return (
+    <>
+      <Container>
+        <ContainerDIV>
+          <span onClick={ccc}>
+            <Btn>
+                <FormatListBulletedRoundedIcon style={{fontSize:14}}/>
+                <span>看板</span>
+                <KeyboardArrowDownRoundedIcon/>
+            </Btn>
+          </span>
+          <span onClick={ccc} style={{marginLeft:'4px'}}>
+            <Btn>
+                <TitleBtn>程式roadMap</TitleBtn>
+            </Btn>
+          </span>
+          <span style={{marginLeft:'4px'}}>
+            <Btn>
+                <StarBorderOutlinedIcon />
+            </Btn>
+          </span>
+          <Border />
+          <sapn>
+            <Btn>
+                <span style={{padding:'0px 4px 0px 4px'}}>jen wen 的工作區</span>
+            </Btn>
+          </sapn>
+          <Border />
+          <span>
+            <Btn>
+              <LockIcon style={{fontSize:14}}/>
+              <span style={{padding:'0px 4px 0px 4px'}}>私密</span>
+            </Btn>
+          </span>
+        </ContainerDIV>
+        <ContainerDIV>
+          <span>
+            <Btn>
+              <RoomServiceIcon/>
+              <span style={{padding:'0px 4px 0px 6px'}}>Bulter</span>
+            </Btn>
+          </span>
+          <span style={{marginLeft:'4px'}}>
+            <Btn>
+              <MoreHorizIcon style={{fontSize:'medium'}}/>
+              <span style={{padding:'0px 4px 0px 6px'}}>顯示選單</span>
+            </Btn>
+          </span>
+        </ContainerDIV>
+      </Container>
+    </>
+  )
 }
